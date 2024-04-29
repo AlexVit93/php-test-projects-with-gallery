bc0b6f75b469a9b4e4310351ade5c15978ee7c8d
AlexVit93 <vitalevicaleks93@gmail.com>
2024-04-29 19:17:27 +0300

    give data to file home

---
+++ assets/pages/home.php
@@ -0,0 +1,7 @@
+<h2 class="text-primary text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem pariatur similique aliquam earum sint? Ea rem accusantium et iure nulla delectus officia ipsam quasi quo hic, porro quia perferendis repellat!
+Deleniti adipisci voluptate cum architecto commodi ut ab tenetur repellendus, in reiciendis quis amet corporis error nulla, id repudiandae quasi impedit vero maiores eveniet? Unde maiores distinctio dolores in aperiam?
+Maiores maxime ut quos eius earum quod consequuntur et molestiae labore fugiat laudantium placeat accusantium, corrupti, nam delectus quia doloribus id? Dignissimos natus voluptates debitis necessitatibus dolorem ipsam suscipit exercitationem!</h2>
+
+<figure>
+<img src="./assets/images/main.jpg" alt="Desert_Electric">
+</figure>
