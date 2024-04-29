734206886ae239aa2311206f955565e6cfa93f32
AlexVit93 <vitalevicaleks93@gmail.com>
2024-04-29 17:51:32 +0300

    fixed first block

--- index.php
+++ index.php
@@ -7,6 +7,14 @@
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
 </head>
 <body>
+    <div class="container col-12 mx-auto">
+        <header class="row">
+            <?php include_once("./assets/pages/header.php");?>
+        </header>
+        <nav class="row">
+            <?php include_once("./assets/pages/menu.php");?>
+        </nav>
+    </div>
 <script src="./assets/js/bootstrap.bundle.js"></script>
 </body>
 </html>
