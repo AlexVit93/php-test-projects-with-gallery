c93b1439f3e64af56d620a121fd93c657fd51045
AlexVit93 <vitalevicaleks93@gmail.com>
2024-04-29 17:46:10 +0300

    add bootstrap to project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Saite</title>
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+</head>
+<body>
+<script src="./assets/js/bootstrap.bundle.js"></script>
+</body>
+</html>
