67ea37840bbfbcd6dec43e72fd79f59d9c4a6177
Antuane_Specione <boosla@gmail.com>
2024-05-04 01:46:13 +0300

    add guestbook

---
+++ assets/pages/formguest.php
@@ -0,0 +1,35 @@
+<?php
+$host = 'localhost'; 
+$db = 'созданная база'; 
+$user = 'root'; 
+$pass = '';  
+$charset = 'utf8mb4';
+
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+// Проверка на отправку формы
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Добавление данных в базу
+    $sql = "INSERT INTO messages (name, email, message) VALUES (?, ?, ?)";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$name, $email, $message]);
+}
+
+// Получение данных из базы
+$stmt = $pdo->query("SELECT name, email, message FROM messages");
+$messages = $stmt->fetchAll();
---
+++ assets/pages/guestbook.php
@@ -0,0 +1,29 @@
+<?php
+include_once("formguest.php")
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Гостевая книга</title>
+<link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+<h3>Гостевая книга</h3>
+<form method="post">
+<input type="text" name="name" placeholder="Ваше имя" required>
+<input type="email" name="email" placeholder="Ваш email" required>
+<textarea name="message" placeholder="Ваше сообщение" required></textarea>
+<button type="submit">Отправить</button>
+</form>
+<h2>Сообщения:</h2>
+    <?php foreach ($messages as $message): ?>
+        <p>
+            <strong><?php echo htmlspecialchars($message['name']); ?></strong><br>
+            <?php echo htmlspecialchars($message['email']); ?><br>
+            <?php echo nl2br(htmlspecialchars($message['message'])); ?>
+        </p>
+    <?php endforeach; ?>
+</body>
+</html>
